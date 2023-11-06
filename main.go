24a268c77a96261abe9da924d092c1f942d21195
Pedro Medina Faria <pedromedina190@gmail.com>
2023-11-06 16:59:59 -0300

    add operations

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var operation string
+	var number1, number2 int
+
+	fmt.Println("Calculator 1.0")
+	fmt.Println("===============")
+	fmt.Println("Which operation do you want to perform? (add, subtract, multiply, divide)")
+	fmt.Scanf("%s", &operation)
+	fmt.Println("Enter the first number")
+	fmt.Scanf("%d", &number1)
+	fmt.Println("Enter the second number")
+	fmt.Scanf("%d", &number2)
+	switch operation {
+	case "add":
+		fmt.Println(number1 + number2)
+	case "subtract":
+		fmt.Println(number1 - number2)
+	case "multiply":
+		fmt.Println(number1 * number2)
+	case "divide":
+		fmt.Println(number1 / number2)
+	}
+}
